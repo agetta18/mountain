98d318855d25e4f907322be940daa7b976bea1fc
Anastasia Koshkina <agetta18@gmail.com>
2020-06-05 14:30:15 +0300

    links and slider

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -7,6 +7,13 @@ function workMenu() {
     const menu = document.querySelector(".menu");
     const btn = document.querySelector('.menu__span');
     const socials = document.querySelector('.socials--mini');
+    
+
+    menu.addEventListener('click', function() {
+        menu.classList.remove('menu--mini');
+        btn.classList.remove('menu__span--exit');
+        socials.style.display='none';
+    })
 
     btn.addEventListener("click", function (e) {
         e.preventDefault();
